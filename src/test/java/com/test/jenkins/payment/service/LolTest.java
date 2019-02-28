6a9a6d1655a306b8962bfaec61ff063c783ee044
Sathyam Lokare <sathyam.lokare@hashedin.com>
2019-02-28 10:25:44 +0530

    logs

--- src/test/java/com/test/jenkins/payment/service/LolTest.java
+++ src/test/java/com/test/jenkins/payment/service/LolTest.java
@@ -2,6 +2,8 @@ package com.test.jenkins.payment.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,6 +14,8 @@ import java.util.Scanner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LolTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LolTest.class);
 
     @Test
     public void test() throws IOException{
@@ -26,6 +30,9 @@ public class LolTest {
     private String readFile(String pathname) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource(pathname).getFile());
+        LOG.info(file.getName());
+        LOG.info(file.getAbsolutePath());
+        LOG.info(file.getPath());
         StringBuilder fileContents = new StringBuilder((int)file.length());
 
         try (Scanner scanner = new Scanner(file)) {
